a8bf93b87bc2335eb2790205f840cc5fa411bb3a
Hilario Gutierrez <hilario.m.gutierrez@gmail.com>
2023-10-26 21:09:40 -0300

    install tailwind

--- frontend/proyect/tailwind.config.js
+++ frontend/proyect/tailwind.config.js
@@ -1,12 +1,34 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    }
   },
   variants: {},
   plugins: [],
