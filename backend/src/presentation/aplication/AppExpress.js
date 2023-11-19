abfda40f862b9952b1f20aa0e7bc962d65604c31
Hilario Gutierrez <hilario.m.gutierrez@gmail.com>
2023-11-19 20:20:38 -0300

    update cors

--- backend/src/presentation/aplication/AppExpress.js
+++ backend/src/presentation/aplication/AppExpress.js
@@ -9,12 +9,6 @@ class AppExpress {
     init () {
         this.app = express();
         
-        const corsOption = {
-            "origin": 'http://localhost:5173',
-            "methods":"GET,HEAD,PUT,PATCH,POST,DELETE" ,
-            "preflightContinue": false ,
-            "optionsSuccessStatus": 200 
-            }
         this.app.use(cors());
 
         this.app.use(express.json());
